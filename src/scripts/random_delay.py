21f6f72ba98b7745a5f3710fd726f45c6ee269e6
Achilleas Seisa <achilleas2942@gmail.com>
2024-05-28 13:58:41 +0200

    Scripts from main branch

---
+++ src/scripts/random_delay.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+
+import rospy
+import random
+import time
+from nav_msgs.msg import Odometry
+from geometry_msgs.msg import TwistStamped
+
+
+class RandomDelayNode:
+    def __init__(self):
+        self.odom_sub = rospy.Subscriber(
+            "/hummingbird/ground_truth/odometry", Odometry, self.callback_odometry
+        )
+        self.cmd_vel_sub = rospy.Subscriber(
+            "/cmd_vel", TwistStamped, self.callback_cmd_vel
+        )
+        self.odom_pub = rospy.Publisher("/odometry", Odometry, queue_size=10)
+        self.cmd_vel_pub = rospy.Publisher(
+            "/mavros/setpoint_attitude/cmd_vel", TwistStamped, queue_size=10
+        )
+
+    def callback_odometry(self, msg):
+        delay = random.uniform(
+            0.005, 0.05
+        )  # Random delay between 0.01 and 0.05 seconds
+        time.sleep(delay)
+        self.odom_pub.publish(msg)
+
+    def callback_cmd_vel(self, msg):
+        delay = random.uniform(
+            0.005, 0.05
+        )  # Random delay between 0.01 and 0.05 seconds
+        time.sleep(delay)
+        self.cmd_vel_pub.publish(msg)
+
+
+if __name__ == "__main__":
+    rospy.init_node("random_delay_node", anonymous=True)
+    node = RandomDelayNode()
+    rospy.spin()
